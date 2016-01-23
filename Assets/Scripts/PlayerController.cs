95d0707db5445532deb637406e85364b27528ad6
Henry Gamba <henrypgamba@gmail.com>
2016-01-23 19:21:15 -0200

    Created script PlayerController
    - player movement and jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2d;
+    public float MovementSpeed;
+    public float JumpHeight;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+        if (rigidbody2d.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        if (rigidbody2d.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(MovementSpeed, rigidbody2d.velocity.y);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(-MovementSpeed, rigidbody2d.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Jump") != 0)
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, JumpHeight);
+        }
+#endif
+    }
+}
